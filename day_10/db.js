e627332c5ef37967e048df8db9e0fe2c5d5cdc47
Ku Nal <104050919+Kunalyadava@users.noreply.github.com>
2023-04-25 03:30:36 +0530

    task added

--- day_10/db.js
+++ day_10/db.js
@@ -1,7 +1,21 @@
 //connection logic // or this is the separate file for connection
 
-const mongoose=require("express")
-const connection=mongoose.connect("mongodb://127.0.0.1.27017/")
-
-
-module.exports={connection}
+const mongoose = require("mongoose");
+const connection = mongoose.connect("mongodb://127.0.0.1:27017/metausers");
+//metausers is databse name
+//user Schema for
+const userSchema = mongoose.Schema({
+  name: String,
+  age: Number,
+  isMarried: Boolean,
+  city: String,
+  language: String
+},{
+    versionKey:false
+});
+//usermodel
+const UserModel = mongoose.model("user", userSchema); //creating model
+module.exports = {
+  connection,
+  UserModel
+};
--- day_10/index.js
+++ day_10/index.js
@@ -1,3 +1,5 @@
+
+
 // Mongoose is a MongoDB object modeling tool designed to work in
 // an asynchronous environment.
 // Mongoose supports Node.js and Deno (alpha).
@@ -7,27 +9,110 @@
 // i  schema  ii model
 // 3 validation
 
-const express = require("express");
-const mongoose = require("mongoose");
-const {connection}=require("./db")
 
+
+
+const express = require("express");
+const { connection } = require("./db");
+const { UserModel } = require("./db");
 const app = express();
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.end("Home page");
+  res.send("Home Page");
 });
-// app.get("/reports",(req,res)=>{
-//     res.end("Reports Page")
+
+
+//create
+
+//post req will insert data to database//data will be presented inside the body so
+app.post("/adduser",async (req, res) => {
+  const payload = req.body;
+  try{
+  const user=new UserModel(payload)
+  await user.save()
+    //when our req is fulfilled it will give status code as well
+  res.status(200).send({"msg":" User data added"}); ///it looks better
+}catch(err){
+  //when our req is fulfilled it will give status code as well
+  res.status(400).send({ "msg": err.msg }); //standardised way of sending response
+}
+});
+
+//Read
+
+
+// app.get("/users",async(req,res)=>{
+//   try{
+//     const users=await UserModel.find()//responsile for reading everything
+//     res.status(200).send(users)
+//   }catch(err){
+//     res.status(400).send({"msg":err.message})
+//   }
 // })
+//  //if we want people of specific city handle the queries in backend// there is no need of separate route for query 
+
+app.get("/users",async(req,res)=>{
+  const query=req.query
+  // console.log(query)
+
+  try{
+    const users=await UserModel.find(query)//to use find as filter command pass query here//
+    // if no query is passes then it will ahow all data 
+    //if the query is to find lang and city then pass it in local host like localhost:8000/users?city=JJP&language=Eng
+    //there is no need to pass city and lang. in line 46
+    //HOW CAN WE ADD AGE MORE THAN 10 OR 20 const users=await UserModel.find({age:{$gte:10}})  (comment out line 42)
+    res.status(200).send(users)
+  }catch(err){
+    res.status(400).send({"msg":err.message})
+  }
+})
+
+
+
+// lets see delete if we want to delete a particular user we need their particular id , 
+//so need params
+//
+//delete
+app.delete("/deleteuser/:userID",async (req,res)=>{
+   const {userID}=req.params
+   try{
+    await UserModel.findByIdAndDelete({_id:userID})
+    res.status(200).send({"msg":" User data deleted"})
+   } catch(err){
+    res.status(400).send({"msg":err.message})
+   }
+   
+})
+//update 
+app.patch("/updateuser/:userID",async (req,res)=>{
+  const {userID}=req.params
+  const payload=req.body
+  try{
+   await UserModel.findByIdAndUpdate({_id:userID},payload)
+   res.status(200).send({"msg":" User data updated"})
+  } catch(err){
+   res.status(400).send({"msg":err.message})
+  }
+  
+})
+//https://mongoosejs.com/docs/queries.html
+
+
+
+
 //whenever server is running ,it should automatically gets connected to mongo
-app.listen(8080, async () => {
+app.listen(8000, async () => {
+    // await mongoose.connect("mongodb://127.0.0.1.27017/"); //connectiong app.listen to mongo //
+    // we are transferring it to db.js file to look more better
   try {
-// await mongoose.connect("mongodb://127.0.0.1.27017/"); //connectiong app.listen to mongo // we are transferring it to db.js file to look more better 
-  await connection 
-console.log("connected to mongo");
+    await connection;
+    //if you want to disconnect //connection.disconnect
+    console.log("connectedto Mongo");
   } catch (err) {
-    console.log("Not anble to connect to mongo");
+    console.log("not able to connect mongodb");
     console.log(err);
   }
-
-  console.log("App running in the port 8080");
+  console.log("app is running on port 8000");
 });
+   //if we want people of specific city handle the queries in backend// there is no need of separate route for query 
