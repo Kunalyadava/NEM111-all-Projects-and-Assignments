ed831209c5175bd3af9be36f2ef10d41991483d7
Ku Nal <104050919+Kunalyadava@users.noreply.github.com>
2023-05-07 20:01:55 +0530

    Update Questions.js
    Signed-off-by: Ku Nal <104050919+Kunalyadava@users.noreply.github.com>

--- Questions.js
+++ Questions.js
@@ -325,3 +325,12 @@
 //  used to authenticate someone's identity, whereas
 //   authorization is a way to provide permission to someone to
 //    access a  particular resource.
+
+/////jwt
+//JWT, or JSON Web Token, is an open standard
+ //used to share security information between
+ //two parties — a client and a server. 
+//Each JWT contains encoded JSON objects, 
+//including a set of claims. JWTs are signed using
+// a cryptographic algorithm to ensure that the 
+//claims cannot be altered after the token is issued.
